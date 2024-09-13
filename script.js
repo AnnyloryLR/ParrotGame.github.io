c28997a61baf6a3118eadaedd6a8c8b8a5ab1f59
Annylory LR <annyloryrosa@gmail.com>
2024-09-13 16:33:57 -0300

    first version

---
+++ script.js
@@ -0,0 +1,73 @@
+figuresCards = ["bobross","explody", "fiesta", "metal", "revertit","triplet", "unicorn"]
+
+function howManyCards(){
+    
+    let cards = prompt("How many cards?");
+
+    condition = (cards%2 === 0 && cards >= 4 && cards <= 14);
+
+    if(condition === false){
+
+        do {
+            cards = prompt("How many cards?")
+            condition = (cards%2 === 0 && cards >= 4 && cards <= 14);
+        }
+
+        while(condition === false); }        
+ 
+
+    return cards;
+}
+
+function compare(){
+    return Math.random() - 0.5;
+}
+
+function selectCards(){
+    let cardsNumber = howManyCards()
+    let shuffled  = figuresCards.sort(compare);
+    figuresCardsDraw = [];
+
+    for(i=0; i< (cardsNumber/2); i++){
+        figuresCardsDraw.push(shuffled[i]);
+        
+
+    }
+
+   return figuresCardsDraw;
+
+}
+
+function cardDealer(){
+   let chosenCards = selectCards()
+   let dealFigureCards = []
+
+  
+    for(i=0; i < chosenCards.length;i++){
+        dealFigureCards.push(chosenCards[i]+"1")
+        dealFigureCards.push(chosenCards[i]+"2")    
+     
+    }
+    
+   return dealFigureCards
+}
+ function dealer(){
+    let dealCards = cardDealer();
+    console.log(dealCards)
+
+    for(i=0;i<dealCards.length; i++){
+        let cardCount = document.querySelector("."+ dealCards[i]);
+        cardCount.classList.remove("hidden");
+        
+    }
+ }
+ dealer()
+
+/*function clickToturn(){}*/
+ 
+/*(document.querySelector("."+ chosenCards[i]+"1")).classList.remove("hidden")
+     //card1.classList.remove("hidden")
+     //console.log("."+ chosenCards[i]+"1")
+     //let card2 = document.querySelector("."+ chosenCards[i]+"2")
+     //card2.classList.remove("hidden")
+     //console.log("."+ chosenCards[i]+"2")*/
